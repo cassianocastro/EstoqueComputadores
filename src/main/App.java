c5d3d295c58559d158db5b41134b1756295f6b26
Cassiano Castro <development.castro@gmail.com>
2024-02-26 16:03:33 -0300

    File addition

---
+++ src/main/App.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import model.DBConfig;
+import model.dao.ConnectionFactory;
+
+/**
+ *
+ */
+public class App
+{
+
+    public void start()
+    {
+        DBConfig config = new DBConfig(null);
+
+        try (Connection connection = new ConnectionFactory().getConnection(config))
+        {
+
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+    }
+}
