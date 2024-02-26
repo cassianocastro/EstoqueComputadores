bc4b5445f6a5e2de670fde886db10ce325b8a2b2
Cassiano Castro <development.castro@gmail.com>
2024-02-26 16:05:33 -0300

    File addition

---
+++ src/lib/dump.sql
@@ -0,0 +1,52 @@
+/**
+ * Created: 20 de mai de 2022
+ */
+
+CREATE TABLE IF NOT EXISTS client (
+    PK_ID     INTEGER PRIMARY KEY,
+    name      TEXT NOT NULL,
+    cpf       TEXT NOT NULL,
+    sex       TEXT NOT NULL,
+    birthDate INTEGER NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    PK_ID     INTEGER PRIMARY KEY,
+    name      TEXT NOT NULL,
+    cpf       TEXT NOT NULL,
+    sex       TEXT NOT NULL,
+    birthDate INTEGER NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS computer (
+    PK_ID      INTEGER PRIMARY KEY,
+    mark       TEXT NOT NULL,
+    model      TEXT NOT NULL,
+    processor  TEXT NOT NULL,
+    ram        INTEGER NOT NULL,
+    storage    INTEGER NOT NULL,
+    type       TEXT NOT NULL,
+    color      TEXT NOT NULL,
+    screenSize REAL NOT NULL
+);
+
+INSERT INTO
+    client(name, cpf, sex, birthDate)
+VALUES
+    (),
+    (),
+    ();
+
+INSERT INTO
+    employee(name, cpf, sex, birthDate)
+VALUES
+    (),
+    (),
+    ();
+
+INSERT INTO
+    computer(mark, model, processor, ram, storage, type, color, screenSize)
+VALUES
+    (),
+    (),
+    ();
